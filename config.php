016b09ec183c09ba28eaefa9666dd8bbdc23ef56
Ashish Yadav <121729128+ashishyadav210@users.noreply.github.com>
2026-02-17 13:31:23 +0530

    Create config.php

---
+++ config.php
@@ -0,0 +1,11 @@
+<?php
+$BOT_TOKEN = "ENTER YOUR BOT TOKEN HERE";
+$ADMIN_ID = 6481253895;
+
+$CHANNEL_ID = "-10018698xxxxx";
+$CHANNEL_LINK = "https://t.me/Unknown_Reason";
+
+$YT = "https://YouTube.com/@Unknown_Reason";
+$IG = "https://Instagram.com/6_hf0";
+
+$API = "https://api.telegram.org/bot$BOT_TOKEN/";
